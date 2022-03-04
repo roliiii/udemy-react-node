3ab210d4ce92d8023e632b4972fd783e89efd3cf
roliiii <roli4759@hotmail.com>
2022-03-04 09:57:59 +0100

    auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,13 @@
-export default function authReducer(state = {}, action) {
+import { FETCH_USER } from "../actions/types";
+
+
+const defaultState = null
+
+export default function authReducer(state = defaultState, action) {
     console.log(action)
     switch (action.type) {
+        case (FETCH_USER): 
+            return action.payload || false;
         default:
             return state;
     }
